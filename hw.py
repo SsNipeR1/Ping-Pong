aecc758658e896fa5cb4a9b178af2f22e79d0b4d
Denis Mitin <denis.mitin.005@gmail.com>
2020-05-06 19:01:22 +0300

    Added ricochet, but you can't play normally because ball sometimes goes ahead.

--- hw.py
+++ hw.py
@@ -17,6 +17,8 @@ BALL_SPEED_UP = 1.00
 BALL_MAX_SPEED = 30
 BALL_X_SPEED = 20
 BALL_Y_SPEED = 20
+PLAYER_1_SCORE = 0
+PLAYER_2_SCORE = 0
 right_line_distance = WIDTH - PAD_W
 
 #tkinter
@@ -37,8 +39,40 @@ LEFT_PAD = c.create_line(PAD_W/2, 0, PAD_W/2, PAD_H, width=PAD_W, fill = "#FF00F
 RIGHT_PAD =  c.create_line(WIDTH-PAD_W/2, 0, WIDTH-PAD_W/2, PAD_H, width = PAD_W, fill = "#FF00FF")
 
 #defs
+def bounce(action):
+    global BALL_X_SPEED, BALL_Y_SPEED
+    if action == "strike":
+        BALL_Y_SPEED = random.randrange(-10, 10)
+        if abs(BALL_X_SPEED) < BALL_MAX_SPEED:
+            BALL_X_SPEED *= -BALL_SPEED_UP
+        else:
+            BALL_X_SPEED = -BALL_X_SPEED
+    else:
+        BALL_Y_SPEED = -BALL_Y_SPEED
+
 def move_ball():
-    c.move(BALL, BALL_X_CHANGE, BALL_Y_CHANGE)
+    ball_left, ball_right, ball_top, ball_bottom = c.coords(BALL)
+    ball_center = (ball_top + ball_bottom)/2
+    if ball_right + BALL_X_SPEED < right_line_distance and ball_left + BALL_X_SPEED > PAD_W:
+        c.move(BALL, BALL_X_SPEED, BALL_Y_SPEED)
+    elif ball_right == right_line_distance or ball_left ==  PAD_W:
+        if ball_right > WIDTH / 2:
+            if c.coords(RIGHT_PAD)[1]< ball_center < c.coords(RIGHT_PAD)[3]:
+                bounce('strike')
+            else:
+                pass
+        else:
+            if c.coords(LEFT_PAD)[1] < ball_center < c.coords(LEFT_PAD)[3]:
+                bounce('strike')
+            else:
+                pass
+    else:
+        if ball_right > WIDTH / 2:
+            c.move(BALL, right_line_distance-ball_right, BALL_Y_SPEED)
+        else:
+            c.move(BALL, -ball_left+PAD_W, BALL_Y_SPEED)
+    if ball_top + BALL_Y_SPEED < 0 or ball_bottom + BALL_Y_SPEED > HEIGHT:
+        bounce('ricochet')
 def move_pads():
     PADS = {LEFT_PAD:LEFT_PAD_SPEED, RIGHT_PAD:RIGHT_PAD_SPEED}
     for pad in PADS:
